e77e1a6a6c4a383542ed8fee1c004502041501c3
Trailee <178453713+trailee@users.noreply.github.com>
2026-03-03 01:45:12 -0500

    credits layout
    - open + close credits panel

--- crab-game/main_menu.gd
+++ crab-game/main_menu.gd
@@ -1,6 +1,20 @@
 extends Control
 
+func _ready():
+	$Credits.hide()
 
 # START BUTTON
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/battle.tscn")
+func _on_start_button_mouse_entered() -> void:
+	$hover_sfx.play()
+
+# CREDITS BUTTON
+func _on_credits_button_pressed() -> void:
+	$Credits.show()
+func _on_credits_button_mouse_entered() -> void:
+	$hover_sfx.play()
+
+# EXIT CREDITS
+func _on_exit_credits_pressed() -> void:
+	$Credits.hide()
