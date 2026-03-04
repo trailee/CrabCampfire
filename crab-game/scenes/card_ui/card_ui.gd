1641c8bbc2075537be1893e5ac8725a69bf64550
BlopDev <118669301+BlopDev@users.noreply.github.com>
2026-03-04 09:49:58 -0500

    Cardsnowcraggable

--- crab-game/scenes/card_ui/card_ui.gd
+++ crab-game/scenes/card_ui/card_ui.gd
@@ -1,7 +1,25 @@
 class_name CardUI
 extends Control
 
+@warning_ignore("unused_signal")
 signal reparent_requested(which_card_ui)
 
 @onready var color: ColorRect = $Color
 @onready var state: Label = $State
+@onready var drop_point_detector: Area2D = $DropPointDetector
+@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
+
+func _ready() -> void:
+	card_state_machine.init(self)
+
+func _input(event: InputEvent) -> void:
+	card_state_machine.on_input(event)
+
+func _on_gui_input(event: InputEvent) -> void:
+	card_state_machine.on_gui_input(event)
+	
+func _on_mouse_entered() -> void:
+	card_state_machine.on_mouse_entered()
+	
+func _on_mouse_exited() -> void:
+	card_state_machine.on_mouse_exited()
