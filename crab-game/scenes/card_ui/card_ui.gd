20e6f1f30ecdb9553244df086cfd0c6bfc38edbe
Trailee <178453713+trailee@users.noreply.github.com>
2026-03-03 22:14:10 -0500

    Revert "Merge branch 'main' of https://github.com/trailee/CrabCampfire"
    This reverts commit 3910d60afd1b99c2ea6d2b0aa60ac7b2ecfdbb0f, reversing
    changes made to 797ff1defb998f8c0ff0066eaa02f00fdfcb11f9.

--- crab-game/scenes/card_ui/card_ui.gd
+++ crab-game/scenes/card_ui/card_ui.gd
@@ -1,24 +1,17 @@
 extends Control
 class_name CardUI
 
-
 signal reparent_requested(which_card_ui: CardUI)
 
 @onready var color: ColorRect = $Color
 @onready var state: Label = $State
-@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	card_state_machine.init(self)
-	
-func _input(event: InputEvent) -> void:
-	card_state_machine.on_input(event)
+	pass # Replace with function body.
 
-func _on_gui_input(event: InputEvent) -> void:
-	card_state_machine.on_gui_input(event)
 
-func _on_mouse_entered() -> void:
-	card_state_machine.on_mouse_entered()
-	
-func _on_mouse_exited() -> void:
-	card_state_machine.on_mouse_exited()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
