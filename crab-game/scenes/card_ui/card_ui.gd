1d66cc4b5bc7af57adf27cd0d336d23fa7b4565f
BlopDev <118669301+BlopDev@users.noreply.github.com>
2026-03-04 10:10:57 -0500

    test

--- crab-game/scenes/card_ui/card_ui.gd
+++ crab-game/scenes/card_ui/card_ui.gd
@@ -8,6 +8,7 @@ signal reparent_requested(which_card_ui)
 @onready var state: Label = $State
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
+@onready var targets: Array[Node] = []
 
 func _ready() -> void:
 	card_state_machine.init(self)
@@ -23,3 +24,11 @@ func _on_mouse_entered() -> void:
 	
 func _on_mouse_exited() -> void:
 	card_state_machine.on_mouse_exited()
+
+
+func _on_drop_point_detector_area_entered(area: Area2D) -> void:
+	if not targets.has(area):
+		targets.append(area)
+
+func _on_drop_point_detector_area_exited(area: Area2D) -> void:
+	targets.erase(area)
