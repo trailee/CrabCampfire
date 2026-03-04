3719932ed8d0de3e3fa77e320f8b177043d2c7f0
BlopDev <118669301+BlopDev@users.noreply.github.com>
2026-03-04 12:08:29 -0500

    cardaim

---
+++ crab-game/global/events.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@warning_ignore("unused_signal")
+signal card_aim_started(card_ui: CardUI)
+
+@warning_ignore("unused_signal")
+signal card_aim_ended(card_ui: CardUI)
--- crab-game/scenes/card_ui/card_ui.gd
+++ crab-game/scenes/card_ui/card_ui.gd
@@ -4,17 +4,26 @@ extends Control
 @warning_ignore("unused_signal")
 signal reparent_requested(which_card_ui)
 
+@export var card: Card
+
 @onready var color: ColorRect = $Color
 @onready var state: Label = $State
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 @onready var targets: Array[Node] = []
 
+var parent: Control
+var tween: Tween
+
 func _ready() -> void:
 	card_state_machine.init(self)
 
 func _input(event: InputEvent) -> void:
 	card_state_machine.on_input(event)
+
+func animate_to_position(new_position: Vector2, duration: float) -> void:
+	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "global_position", new_position, duration)
 
 func _on_gui_input(event: InputEvent) -> void:
 	card_state_machine.on_gui_input(event)
